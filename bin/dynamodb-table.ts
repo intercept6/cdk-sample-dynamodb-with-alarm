5f415c49f27d99d32862a2a1226ea708721fc41f
intercept6 <intercept6@gmail.com>
2020-02-04 15:40:14 +0900

    Initial commit

---
+++ bin/dynamodb-table.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { DynamodbTableStack } from '../lib/dynamodb-table-stack';
+
+const app = new cdk.App();
+new DynamodbTableStack(app, 'DynamodbTableStack');
